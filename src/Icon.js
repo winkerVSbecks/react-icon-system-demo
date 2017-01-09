1b36e790bfe03df65cec198d6b14671eff1a3d50
Varun Vachhar <varunvachhar@gmail.com>
2017-01-09 10:52:59 -0500

    📚

--- src/Icon.js
+++ src/Icon.js
@@ -4,7 +4,7 @@ files.keys().forEach(files);
 
 const Icon = ({ type, className }) => (
   <svg viewBox="0 0 20 20"
-    className={ className }
+    className={ `v-mid ${className}` }
     width="1em" height="1em">
     <use xlinkHref={ `#${ type }` }></use>
   </svg>
