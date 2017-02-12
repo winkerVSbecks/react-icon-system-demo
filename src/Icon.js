6e66ef8ab9ce5f4344c3a86de3d31f808910a877
Varun Vachhar <varunvachhar@gmail.com>
2017-02-12 18:30:37 -0500

    📚

--- src/Icon.js
+++ src/Icon.js
@@ -3,9 +3,8 @@ const files = require.context('!svg-sprite!./assets', false, /.*\.svg$/);
 files.keys().forEach(files);
 
 const Icon = ({ type, className }) => (
-  <svg viewBox="0 0 20 20"
-    className={ `v-mid ${className}` }
-    width="1em" height="1em">
+  <svg className={ `dib v-mid ${ className }` }
+    height="1em" width="1em">
     <use xlinkHref={ `#${ type }` }></use>
   </svg>
 );
